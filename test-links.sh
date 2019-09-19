83d47bfba7efa3a82c746a939f81013ec11cf22b
Michal Hrusecky <michal.hrusecky@nic.cz>
2019-09-19 18:42:37 +0200

    Test remote links as well
    Internal documentation links are hard fail, but let remote links fail, although
    we would like to know about it.

--- test-links.sh
+++ test-links.sh
@@ -3,9 +3,15 @@ set -e
 
 PORT=8001
 PID=""
+if [ x"$1" = x--remote ]; then
+	REMOTE="yes"
+else
+	REMOTE=""
+fi
 
 cleanup() {
 	[ -z "$PID" ] || kill "$PID"
+	rm -f rejected todo
 }
 trap cleanup EXIT
 
@@ -29,4 +35,8 @@ running || {
 }
 
 # Crawler trough links on website (note that local only)
-wget --spider --recursive --delete-after "http://localhost:$PORT/"
+wget --no-verbose --rejected-log=rejected --spider --recursive --page-requisites --level=inf --delete-after "http://localhost:$PORT/"
+if [ -n "$REMOTE" ]; then
+	tail -n +2 rejected | cut -f 2 | grep -v -E '(turris.local|192.168.1.1|127.0.0.1|localhost)' | sort -u | sed -e 's|%3A|:|g' > todo
+	wget --no-verbose --spider --input-file=todo --delete-after
+fi
