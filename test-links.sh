623ff15db83e358e06b63c2a6db5647d005d718f
Michal Hrusecky <michal.hrusecky@turris.com>
2021-10-13 12:23:10 +0200

    test-links: Improve the readability of output
    Ignore release template URL as it is not possible to resolve it without
    filling it up. Also report failed URLs at the end of test to make it
    easier to go through them.

--- test-links.sh
+++ test-links.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 PORT=8001
@@ -11,7 +11,14 @@ fi
 
 cleanup() {
 	[ -z "$PID" ] || kill "$PID"
-	rm -f rejected todo
+	if [ "$(grep -v '200 OK$' log | wc -l)" -gt 0 ]; then
+		echo "---------------"
+		echo "|  Failures:  |"
+		echo "---------------"
+		echo
+		grep -B 1 -v '200 OK$' log
+	fi
+	rm -f rejected todo log
 }
 trap cleanup EXIT
 
@@ -37,6 +44,7 @@ running || {
 # Crawler trough links on website (note that local only)
 wget --no-verbose --rejected-log=rejected --spider --recursive --page-requisites --level=inf --no-directories --delete-after "http://localhost:$PORT/"
 if [ -n "$REMOTE" ]; then
-	tail -n +2 rejected | cut -f 2 | grep -v -E '(turris.local|192.168.1.1|127.0.0.1|localhost)' | sort -u | sed -e 's|%3A|:|g' > todo
-	wget --no-verbose --spider --input-file=todo --no-directories --delete-after --user-agent="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"
+	tail -n +2 rejected | cut -f 2 | grep -v -E '(turris.local|gitlab.nic.cz/turris/os/build/tree/.*release.name.*|192.168.1.1|127.0.0.1|localhost)' | sort -u | sed -e 's|%3A|:|g' > todo
+    set -o pipefail
+	wget --no-verbose --spider --input-file=todo --no-directories --delete-after --user-agent="Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" 2>&1 | tee log
 fi
